8a9a227aaf5a889aa99d075586c4b1ff2d7743dc
mix <ivanmix_88@mail.ru>
2016-03-22 09:08:39 +0200

    D3

--- src/main/java/com/ivanmix/controller/CountController.java
+++ src/main/java/com/ivanmix/controller/CountController.java
@@ -5,6 +5,8 @@ import com.ivanmix.model.User;
 import com.ivanmix.service.UserService;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,27 +21,66 @@ import java.util.List;
 
 public class CountController extends HttpServlet{
 
-    private int countTwo;
     private volatile int countOne;
+    private int countTwo;
+    private volatile int countThree;
+    private int countFour;
 
     public void init()
     {
         countOne = 0;
         countTwo = 0;
+        countThree = 0;
+        countFour = 0;
+    }
+
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+        countOne++;
+        countFour++;
+
+        if(req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+            HttpServletRequest request = (HttpServletRequest)req;
+            HttpServletResponse response = (HttpServletResponse)res;
+            this.service(request, response);
+        } else {
+            throw new ServletException("non-HTTP request or response");
+        }
     }
 
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        countOne++;
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
         synchronized( this ) {
             countTwo++;
         }
+
         resp.setContentType("text/html");
         Writer writer = resp.getWriter();
         writer.append("<h1><a href='/'>Dreans Jod</a></h1>");
         writer.append("<h2>countOne: " + countOne + "</h2>");
         writer.append("<h2>countTwo: " + countTwo + "</h2>");
+        writer.append("<h2>countThree: " + countThree + "</h2>");
+        writer.append("<h2>countFour: " + countFour + "</h2>");
+        resp.setContentType("text/html");
+        writer.append("<form action=\"/count\" method=\"POST\">");
+        writer.append("<input type=\"submit\" value=\"Submit\" />");
+        writer.append("</form>");
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        countThree++;
+        resp.setContentType("text/html");
+        Writer writer = resp.getWriter();
+        writer.append("<h1><a href='/'>Dreans Jod</a></h1>");
+        writer.append("<h2>countOne: " + countOne + "</h2>");
+        writer.append("<h2>countTwo: " + countTwo + "</h2>");
+        writer.append("<h2>countThree: " + countThree + "</h2>");
+        writer.append("<h2>countFour: " + countFour + "</h2>");
         writer.flush();
     }
 
